cf5699349242e6f3e621a3e3675d7701672d40ca
tamim-hossain <tamim_hossain@outlook.com>
2021-03-24 19:38:31 +0600

    almost done

---
+++ src/components/Header.jsx
@@ -0,0 +1,97 @@
+import { Button, Grid, makeStyles, Typography } from "@material-ui/core";
+import handImg from "../assets/hand.svg";
+
+const useStyles = makeStyles({
+	header: {
+		background: "#111011",
+	},
+	headerRight: {
+		padding: "15px",
+		paddingBottom: "100px",
+	},
+	text: {
+		color: "white",
+	},
+	btn: {
+		background: "yellow",
+		fontWeight: "bold",
+		marginRight: "20px",
+		"&:hover": {
+			background: "cyan",
+		},
+	},
+	typo: {
+		marginTop: "25px",
+		marginBottom: "70px",
+	},
+	img: {
+		height: "100vh",
+	},
+	menu: {
+		color: "#FFFFFF",
+		fontWeight: "bold",
+		padding: "5px 20px 5px 20px",
+		marginTop: "20px",
+	},
+	btnGrp: {
+		marginRight: "25px",
+	},
+});
+
+const Header = () => {
+	const classes = useStyles();
+
+	return (
+		<Grid container className={classes.header}>
+			<Grid item md={6}>
+				<img src={handImg} alt="" className={classes.img} />
+			</Grid>
+			<Grid item md={6} container>
+				<Grid item container justify="flex-end" className={classes.btnGrp}>
+					<Grid>
+						<Button disableElevation className={classes.menu}>
+							Services
+						</Button>
+					</Grid>
+					<Grid>
+						<Button disableElevation className={classes.menu}>
+							Products
+						</Button>
+					</Grid>
+					<Grid>
+						<Button disableElevation className={classes.menu}>
+							About
+						</Button>
+					</Grid>
+					<Grid>
+						<Button disableElevation className={classes.menu}>
+							Careers
+						</Button>
+					</Grid>
+				</Grid>
+				<Grid item className={classes.headerRight} container direction="column" justify="center">
+					<Typography variant="h5" component="h2" className={classes.text} gutterBottom>
+						Touch the future with AB products.
+					</Typography>
+					<Typography className={classes.text}>
+						Make your workspace visionary with AB solutions <br /> and services.
+					</Typography>
+					<Typography className={`${classes.text} ${classes.typo}`}>
+						Execute your ideas with our solutions and execution team. <br /> Make a strong bond together &amp; make the
+						future era a better place.
+					</Typography>
+					<Grid>
+						<Button variant="contained" className={classes.btn}>
+							PRODUCTS DEMO
+						</Button>
+						<Button variant="contained" className={classes.btn}>
+							SEE BLOGS
+						</Button>
+					</Grid>
+				</Grid>
+			</Grid>
+		</Grid>
+	);
+};
+
+export default Header;
