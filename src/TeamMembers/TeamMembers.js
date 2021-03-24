d2e7e2e99f0b3495a0fb35900ae80a6904b68737
basir5101 <basir.bsmrstu@gmail.com>
2021-03-24 17:53:49 +0600

    team-member-section-added

---
+++ src/TeamMembers/TeamMembers.js
@@ -0,0 +1,70 @@
+import React from 'react';
+import OwlCarousel from 'react-owl-carousel';
+import 'owl.carousel/dist/assets/owl.carousel.css';
+import 'owl.carousel/dist/assets/owl.theme.default.css';
+import demoImage from '../assets/placeholder image.jpg';
+import './TeamMembers.css'
+
+const TeamMembers = () => {
+
+    const responsiveObject = {
+        0: {
+            items: 1,
+        },
+        450: {
+            items: 2,
+        },
+        600: {
+            items: 3,
+        },
+        1000: {
+            items: 4,
+        },
+    }
+
+    return (
+        <section className='team-member'>
+            <h3 className>OUR CORE TEAM MEMBERS</h3>
+            <OwlCarousel responsive={responsiveObject} items={4} center autoplay autoplaySpeed={1000} slidetransition={'linear'} autoplayTimeout={4000} loop mouseDrag margin={10}>
+                <div className='item'>
+                    <div className='carousel-item'>
+                        <img src={demoImage} alt="" />
+                        <p>Sayantan Kirtaniya</p>
+                    </div>
+                </div>
+                <div className='item'>
+                    <div className='carousel-item'>
+                        <img src={demoImage} alt="" />
+                        <p>Sayantan Kirtaniya</p>
+                    </div>
+                </div>
+                <div className='item'>
+                    <div className='carousel-item'>
+                        <img src={demoImage} alt="" />
+                        <p>Sayantan Kirtaniya</p>
+                    </div>
+                </div>
+                <div className='item'>
+                    <div className='carousel-item'>
+                        <img src={demoImage} alt="" />
+                        <p>Sayantan Kirtaniya</p>
+                    </div>
+                </div>
+                <div className='item'>
+                    <div className='carousel-item'>
+                        <img src={demoImage} alt="" />
+                        <p>Sayantan Kirtaniya</p>
+                    </div>
+                </div>
+                <div className='item'>
+                    <div className='carousel-item'>
+                        <img src={demoImage} alt="" />
+                        <p>Sayantan Kirtaniya</p>
+                    </div>
+                </div>
+            </OwlCarousel>;
+        </section>
+    );
+};
+
+export default TeamMembers;
