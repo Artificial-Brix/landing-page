1e8f358d9e21b3beb232a75954cc80f09e65dd5f
Tamim-Hossain <67548848+Tamim-Hossain@users.noreply.github.com>
2021-03-27 12:25:54 +0600

    review section added on homepage (#9)
    * review section done
    
    * review section added on homepage
    
    Co-authored-by: tamim-hossain

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
-import './App.css';
+import "./App.css";
 import Header from "./components/header/Header";
-import TeamMembers from './components/TeamMembers/TeamMembers';
-import MainHeader from './components/MainHeader/MainHeader';
+import MainHeader from "./components/MainHeader/MainHeader";
+import Review from "./components/review/Review";
+import TeamMembers from "./components/TeamMembers/TeamMembers";
 
 function App() {
-  return (
-    <div className="app">
-      <MainHeader /> 
-      <Header />
-      <TeamMembers />
-    </div>
-  );
+	return (
+		<div className="app">
+			<MainHeader />
+			<Header />
+			<TeamMembers />
+			<Review />
+		</div>
+	);
 }
 
 export default App;
