b5f89d5c81dfc3fdcb68c18ac1c8f1d95e8c7c3f
tamim-hossain <tamim_hossain@outlook.com>
2021-03-27 12:11:03 +0600

    review section done

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
-import './App.css';
-import NavBar from './components/NavBar/NavBar';
-import Slider from './components/Slider/Slider';
+import "./App.css";
 import Header from "./components/header/Header";
-
+import NavBar from "./components/NavBar/NavBar";
+import Review from "./components/review/Review";
+import Slider from "./components/Slider/Slider";
 
 function App() {
-  return (
-    <div className="app">
-      <Slider /> 
-      <NavBar /> 
-      <Header />
-    </div>
-  );
+	return (
+		<div className="app">
+			<Slider />
+			<NavBar />
+			<Header />
+			<Review />
+		</div>
+	);
 }
 
 export default App;
---
+++ src/components/review/ReviewData.js
@@ -0,0 +1,22 @@
+const reviewData = [
+	{
+		title: "Efficient Collaborating",
+		comment: "They are much more reliable to work with.",
+		name: "Uday Gosh",
+		occupation: "Deluxe Orthopaedics",
+	},
+	{
+		title: "Intuitive Design",
+		comment: "Customer satisfaction is as good as always.",
+		name: "Anamika Kirtaniya",
+		occupation: "Freelance Beautician",
+	},
+	{
+		title: "Mind Blowing Service",
+		comment: "Provably the best company to work with.",
+		name: "Dipendra Nath Dey",
+		occupation: "Freelance Electronics Engineer",
+	},
+];
+
+export default reviewData;
