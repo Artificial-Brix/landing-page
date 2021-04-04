9028776ac029aeeb2587750e60125e0f01e34023
masud309060 <masud306090@gmail.com>
2021-04-04 15:04:02 +0600

    done done

--- src/App.js
+++ src/App.js
@@ -1,22 +1,18 @@
 import "./App.css";
-import Footer from "./components/footer/Footer";
 import Header from "./components/header/Header";
 import MainHeader from "./components/MainHeader/MainHeader";
-import Offersection from "./components/offerSection/Offersection";
 import Review from "./components/review/Review";
 import TeamMembers from "./components/TeamMembers/TeamMembers";
 
 function App() {
-  return (
-    <div className="app">
-      <MainHeader />
-      <Header />
-      <Offersection />
-      <TeamMembers />
-      <Review />
-      <Footer />
-    </div>
-  );
+	return (
+		<div className="app">
+			<MainHeader />
+			<Header />
+			<TeamMembers />
+			<Review />
+		</div>
+	);
 }
 
 export default App;
--- src/components/TeamMembers/TeamMembers.js
+++ src/components/TeamMembers/TeamMembers.js
@@ -28,7 +28,7 @@ const TeamMembers = () => {
             <h1>OUR CORE TEAM MEMBERS</h1>
             <OwlCarousel 
             responsive={responsiveObject} 
-            dots={true} items={4} 
+            dots={true} items={4}
             center autoplay autoplaySpeed={1000} 
             slidetransition={'linear'} 
             autoplayTimeout={4000} 
