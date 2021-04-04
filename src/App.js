7e2f956cd54ad8c6cbaf83b3fa2796ae8e0521fc
masud309060 <masud306090@gmail.com>
2021-04-04 15:06:40 +0600

    done done

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import "./App.css";
+import Footer from "./components/footer/Footer";
 import Header from "./components/header/Header";
 import MainHeader from "./components/MainHeader/MainHeader";
+import Offersection from "./components/offerSection/Offersection";
 import Review from "./components/review/Review";
 import TeamMembers from "./components/TeamMembers/TeamMembers";
 
@@ -9,8 +11,10 @@ function App() {
 		<div className="app">
 			<MainHeader />
 			<Header />
+			<Offersection /> 
 			<TeamMembers />
 			<Review />
+			<Footer /> 
 		</div>
 	);
 }
